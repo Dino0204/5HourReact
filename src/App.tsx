89939f90f045bb02f6405c9a312c9593df3a9250
Dino0204 <s24020@gsm.hs.kr>
2024-10-09 00:29:01 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import MainPage from '@pages/index'
+import AboutPage from '@pages/about'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path='/' element={<MainPage/>}></Route>
+        <Route path='/about' element={<AboutPage/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      About 홈페이지 입니다.
+    </div>
+  )
+}
+
+export default About

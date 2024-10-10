364a47dcbf36001a384932b0edbc0da4fc112ce1
Dino0204 <s24020@gsm.hs.kr>
2024-10-10 23:50:00 +0900

    fix: Error Refectoring

--- src/components/common/navigation/CommonNav.tsx
+++ src/components/common/navigation/CommonNav.tsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom';
 import styles from './CommonNav.module.scss'
 import navJson from './nav.json'
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { pageState } from '@/recoil/atoms/pagaState';
+import { searchState } from '@/recoil/atoms/searchState';
 
 interface Navigation{
   index : number,
@@ -12,12 +15,28 @@ interface Navigation{
 }
 
 function CommonNav() {
+  const location = useLocation()
   const [navigation,setNavigation] = useState<Navigation[]>(navJson); // Label 반복문으로 표시
+  const [page,setPage] = useRecoilState(pageState)
+  const [search,setSearch] = useRecoilState(searchState)
+
+  useEffect(() => { 
+    navigation.forEach((nav:Navigation) => {
+      nav.isActive = false
+
+      if(nav.path === location.pathname || location.pathname.includes(nav.path)){
+        nav.isActive = true
+        setSearch(nav.searchValue)
+        setPage(1)
+      }
+    })
+    setNavigation([...navigation])
+  },[location.pathname])
 
   // useState 반응성 데이터 반복 호출
   const navLinks = navigation.map((item:Navigation) => {
     return(      
-      <Link to={item.path} className={styles.navigation_menu} key={item.path}>
+      <Link to={item.path} className={ item.isActive ?  `${styles.navigation_menu} ${styles.active}` : `${styles.navigation_menu} ${styles.inactive}`} key={item.path}>
         <span className={styles.navigation_menu_label}>{item.label}</span>
       </Link>
     )
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useMemo, useState } from 'react';
+import { useRecoilValueLoadable } from 'recoil';
 import { imageData } from '@/recoil/selectors/imageSelectors';
 
 import CommonHeader from '@/components/common/header/CommonHeader';
@@ -14,15 +14,23 @@ import styles from './styles/index.module.scss';
 
 
 function Index() {
-  const imgSelector = useRecoilValue(imageData)
+  const imgSelector = useRecoilValueLoadable(imageData)
   const [imgData,setImgData] = useState<CardDTO>();
   const [open,setOpen] = useState<boolean>(false) // 이미지 상세 다이얼로그 (모달)
 
-  const CARD_LIST = imgSelector.data.results.map((card:CardDTO)=>{
-    return(
-      <Card data={card} key = {card.id} handleDialog={setOpen} handleSetData={setImgData}/>
-    )
-  })
+  // 반복적으로 사용되는 부분을 저장 (계산된 데이터로 저장) | 읽기쓰기 모두 가능한 훅
+  const CARD_LIST = useMemo(() => {
+    if(imgSelector.state === "hasValue"){
+      const result = imgSelector.contents.map((card:CardDTO)=>{
+        return <Card data={card} key = {card.id} handleDialog={setOpen} handleSetData={setImgData}/>   
+      })
+      return result
+    }
+    // imgSelector.state === "hasLoading"
+    else{
+      return <div>Loading...</div>
+    }
+  },[imgSelector])
 
   return (
     <div className={styles.page}>

62503391dcea31679e27957e88e79e90db751c12
Dino0204 <s24020@gsm.hs.kr>
2024-10-11 08:07:13 +0900

    fix: SearchBar Logic

--- src/components/common/searchBar/CommonSearchBar.tsx
+++ src/components/common/searchBar/CommonSearchBar.tsx
@@ -1,11 +1,43 @@
+import { useState } from 'react'
 import styles from './CommonSearchBar.module.scss'
+import { useRecoilState } from 'recoil'
+import { searchState } from '@/recoil/atoms/searchState'
 
 function CommonSearchBar() {
+  const [search,setSearch] = useRecoilState(searchState)
+  const [text,setText] = useState("")
+  const onChange =  (event) => {
+    setText(event.target.value)
+  }
+
+  const onSearch = () => {
+    if(text === ""){
+      // default
+      setSearch('Korea')
+    }else{
+      // Input Value
+      setSearch(text)
+    }
+  }
+  
+  // 키보드 
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if(event.key === "Enter"){
+      if(text === ""){
+        // default
+        setSearch('Korea')
+      }else{
+        // Input Value
+        setSearch(text)
+      }
+    }
+  }
+
   return (
     <div className={styles.searchBar}>
       <div className={styles.searchBar_search}>
-        <input type='text' placeholder='찾으실 이미지를 검색하세요.' className={styles.searchBar_search_input}></input>
-        <img src='src\assets\icons\icon-search.svg' alt='search'></img>
+        <input type='text' placeholder='찾으실 이미지를 검색하세요.' className={styles.searchBar_search_input} onChange={onChange} onKeyDown={handleKeyDown} value={text}></input>
+        <img src='src\assets\icons\icon-search.svg' alt='search' onClick={onSearch}></img>
       </div> 
     </div>
   )

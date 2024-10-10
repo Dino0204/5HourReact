7d27dfa074868e8a304b1f5cba8b3bf8994c2049
Dino0204 <s24020@gsm.hs.kr>
2024-10-10 20:55:37 +0900

    feat: DetailDialog 파일 추가

---
+++ src/components/common/dialog/DetailDialog.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './DetailDialog.module.scss'
+
+
+function DetailDialog() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DetailDialog

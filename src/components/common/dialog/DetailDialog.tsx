90b4cb57b23e8518dc16467aff5a149c617bc43e
Dino0204 <s24020@gsm.hs.kr>
2024-10-14 19:58:12 +0900

    fix: Refectoring3

--- src/components/common/dialog/DetailDialog.tsx
+++ src/components/common/dialog/DetailDialog.tsx
@@ -1,100 +1,126 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { CardDTO } from '@/pages/index/types/card'
 import styles from './DetailDialog.module.scss'
-import { CardDTO, Tag } from '@/pages/index/types/card'
-import toast, {toastConfig} from 'react-simple-toasts'
+import toast, { toastConfig } from 'react-simple-toasts'
+import 'react-simple-toasts/dist/style.css';
+// import 'react-simple-toasts/dist/theme/dark.css'
 
-toastConfig({theme : 'dark'})
+toastConfig({ theme: 'dark' })
 
 interface Props {
-  data: CardDTO
-  handleDialog: (eventValue: boolean) => void
+    data: CardDTO
+    handleDialog: (eventValue: boolean) => void
 }
 
-function DetailDialog({data,handleDialog}: Props) {
-  
-  const [bookmark,setBookmark] = useState(false)
+function DetailDialog({ data, handleDialog }: Props) {
+    const [bookmark, setBookmark] = useState(false)
+    // 다이얼로그 끄기
+    const closeDialog = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        handleDialog(false)
+        event.stopPropagation()
+    }
+    // 북마크 추가 이벤트
+    const addBookmark = (selected: CardDTO) => {
+        setBookmark(true)
 
-  const closeDialog = () => {
-    handleDialog(false)
-  }
+        const getLocalStorage = JSON.parse(localStorage.getItem('bookmark'))
+        // 1. 로컬스토리지에 bookmark이라는 데이터가 없을 경우
+        if (!getLocalStorage || getLocalStorage === null) {
+            localStorage.setItem('bookmark', JSON.stringify([selected]))
+            toast('해당 이미지를 북마크에 저장하였습니다. 😄')
+        } else {
+            // 2. 해당 이미지가 이미 로컬스토리지 bookmark라는 데이터에 저장되어 있을 경우
+            if (getLocalStorage.findIndex((item: CardDTO) => item.id === selected.id) > -1) {
+                toast('해당 이미지는 이미 북마크에 추가된 상태입니다. ❌')
+            } else {
+                // 3. 해당 이미지가 로컬스토리지 bookmark라는 데이터에 저장되어 있지 않을 경우 + bookmark라는 데이터에 이미 어떤 값이 담겨 있는 경우
+                const res = [...getLocalStorage]
+                res.push(selected)
+                localStorage.setItem('bookmark', JSON.stringify(res))
 
-  const addBookmark = (selected : CardDTO) =>{
-    setBookmark(true)
-
-    const getLocalStorage = JSON.parse(localStorage.getItem("bookmark"))
-
-    // 1. 로컬스토리지에 북마크 저장 데이터 없을 경우
-    if(!getLocalStorage || getLocalStorage === null){
-      localStorage.setItem("bookmark",JSON.stringify([selected]))
-      toast("해당 이미지를 북마크에 저장 하였습니다.")
-    }else{
-      // 2. 해당 이미지가 로컬스토리지에 저장되어 있을 경우
-
+                toast('해당 이미지를 북마크에 저장하였습니다. 😄')
+            }
+        }
     }
 
-  }
+    useEffect(() => {
+        const getLocalStorage = JSON.parse(localStorage.getItem('bookmark'))
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.container_dialog}>
-        <header className={styles.container_dialog_header}>
-          <div className={styles.close}>
-            <button className={styles.close_button} onClick={closeDialog}>
-              {/*Google Icon*/}
-              <span className="material-symbols-outlined" style={{fontSize:28 + "px"}}>
-                close
-              </span>
-            </button>
-            <img src={data.user.profile_image.small} alt="authorProfileImg" className={styles.close_authorImage}/>
-            <span className={styles.close_authorName}>
-              {data.user.name}
-            </span>
-          </div>
-          <div className={styles.bookmark}>
-            <button className={styles.bookmark_button} onClick={() => addBookmark(data)}>
-              {/*Google Icon*/}
-              {bookmark === false ? (
-                <span className="material-symbols-outlined" style={{fontSize:16 + "px"}}>
-                  favorite
-                </span>
-              ) : (
-                <span className="material-symbols-outlined" style={{fontSize:16 + "px", color: 'red'}}> ` `
-                  favorite
-                </span>
-              )}
-              북마크
-            </button>
-            <button className={styles.bookmark_button}>다운로드</button>
-          </div>
-        </header>
-        <main className={styles.container_dialog_body}>
-          <img src={data.urls.small} alt="detailImage" className={styles.image}/>
-        </main>
-        <footer className={styles.container_dialog_footer}>
-          <div className={styles.infoBox}>
-            <div className={styles.infoBox_item}>
-              <span className={styles.infoBox_item_label}>이미지 크기</span>
-              <span className={styles.infoBox_item_value}>
-                {data.width}X{data.width}
-              </span>
+        if (getLocalStorage && getLocalStorage.findIndex((item: CardDTO) => item.id === data.id) > -1) {
+            setBookmark(true)
+        } else if (!getLocalStorage) return
+
+        // ESC Key 입력시, 다이얼로그 닫기
+        const escKeyDownCloseDialog = (event: any) => {
+            console.log('함수호출')
+            if (event.key === 'Escape') {
+                closeDialog(event)
+            }
+        }
+        // ESC Key를 눌렀을 때, 다이얼로그창 닫기
+        window.addEventListener('keydown', escKeyDownCloseDialog) // 위에 만들어 놓은 escKeyDownCloseDialog를 keydown했을 때, 이벤트로 등록한다.
+        return () => window.removeEventListener('keydown', escKeyDownCloseDialog)
+    }, [])
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.container_dialog}>
+                <div className={styles.container_dialog_header}>
+                    <div className={styles.close}>
+                        <button className={styles.close_button} onClick={closeDialog}>
+                            {/* 구글 아이콘을 사용 */}
+                            <span className="material-symbols-outlined" style={{ fontSize: 28 + 'px' }}>
+                                close
+                            </span>
+                        </button>
+                        <img src={data.user.profile_image.small} alt="사진작가 프로필 사진" className={styles.close_authorImage} />
+                        <span className={styles.close_authorName}>{data.user.name}</span>
+                    </div>
+                    <div className={styles.bookmark}>
+                        <button className={styles.bookmark_button} onClick={() => addBookmark(data)}>
+                            {/* 구글 아이콘을 사용 */}
+                            {bookmark === false ? (
+                                <span className="material-symbols-outlined" style={{ fontSize: 16 + 'px' }}>
+                                    favorite
+                                </span>
+                            ) : (
+                                <span className="material-symbols-outlined" style={{ fontSize: 16 + 'px', color: 'red' }}>
+                                    favorite
+                                </span>
+                            )}
+                            북마크
+                        </button>
+                        <button className={styles.bookmark_button}>다운로드</button>
+                    </div>
+                </div>
+                <div className={styles.container_dialog_body}>
+                    <img src={data.urls.small} alt="상세이미지" className={styles.image} />
+                </div>
+                <div className={styles.container_dialog_footer}>
+                    <div className={styles.infoBox}>
+                        <div className={styles.infoBox_item}>
+                            <span className={styles.infoBox_item_label}>이미지 크기</span>
+                            <span className={styles.infoBox_item_value}>
+                                {data.width} X {data.height}
+                            </span>
+                        </div>
+                        <div className={styles.infoBox_item}>
+                            <span className={styles.infoBox_item_label}>업로드</span>
+                            <span className={styles.infoBox_item_value}>{data.created_at.split('T')[0]}</span>
+                        </div>
+                        <div className={styles.infoBox_item}>
+                            <span className={styles.infoBox_item_label}>마지막 업데이트</span>
+                            <span className={styles.infoBox_item_value}>{data.updated_at.split('T')[0]}</span>
+                        </div>
+                        <div className={styles.infoBox_item}>
+                            <span className={styles.infoBox_item_label}>다운로드</span>
+                            <span className={styles.infoBox_item_value}>{data.likes}</span>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={styles.infoBox_item}>
-              <span className={styles.infoBox_item_label}>업로드</span>
-              <span className={styles.infoBox_item_value}>{data.created_at.split("T")[0]}</span>
-            </div>
-            <div className={styles.infoBox_item}>
-              <span className={styles.infoBox_item_label}>마지막 업데이트</span>
-              <span className={styles.infoBox_item_value}>{data.updated_at.split("T")[0]}</span>
-            </div>
-            <div className={styles.infoBox_item}>
-              <span className={styles.infoBox_item_label}>다운로드</span>
-              <span className={styles.infoBox_item_value}>{data.likes}</span>
-            </div>
-          </div>
-        </footer>
-      </div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default DetailDialog
--- src/pages/bookmark/index.tsx
+++ src/pages/bookmark/index.tsx
@@ -1,27 +1,34 @@
-import CommonHeader from '@/components/common/header/CommonHeader'
-import styles from './styles/index.module.scss'
 import { useEffect, useState } from 'react'
-import Card from './components/card'
+import CommonHeader from '@/components/common/header/CommonHeader'
+import Card from './components/Card'
+// CSS
+import styles from './styles/index.module.scss'
+import { CardDTO } from '../index/types/card'
 
 function index() {
-  const[data,setData] = useState([])
-  const getData = () => {
+    const [data, setData] = useState([])
+    const getData = () => {
+        const getLocalStorage = JSON.parse(localStorage.getItem('bookmark'))
 
-  }
+        if (getLocalStorage || getLocalStorage !== null) setData(getLocalStorage)
+        else setData([])
+    }
 
-  useEffect(() => {
-    getData()
-  },[])
+    useEffect(() => {
+        getData()
+    }, [])
 
-  return (
-    <div className={styles.page}>
-      {/* 공통헤더 UI */}
-      <CommonHeader/>
-      <main className={styles.page_contents}>
-        <Card/>
-      </main>
-    </div>
-  )
+    return (
+        <div className={styles.page}>
+            {/* 공통 헤터 UI 부분 */}
+            <CommonHeader />
+            <main className={styles.page_contents}>
+              {data.length === 0 ? 
+              (<div className={styles.page_contents_noData}>조회 가능한 데이터가 없습니다.</div>) : 
+              (data.map((item:CardDTO) => {return <Card prop = {item} key={item.id} />}))}
+            </main>
+        </div>
+    )
 }
 
 export default index
